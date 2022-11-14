96d48f05e391575bcfdfb5ed0780ba8ada4b6cc9
ggajardo90 <69556067+ggajardo90@users.noreply.github.com>
2022-11-13 21:31:56 -0300

    actualizacion del proyecto

--- js/app.js
+++ js/app.js
@@ -1,31 +1,46 @@
-    
-    const cardProduct = (product) => (`
+const URI_BACKEND = "http://127.0.0.1:8000";
+
+const cardProduct = (product) => (`
     <div class="col-md-4 my-2" >
     <div class="card">
-    <img style="height:350px;" src="${product.url_image?product.url_image:'/img/not_found_image.png'}" class="card-img-top" alt="imagen de producto">
+    <img style="height:350px;" src="${product.url_image ? product.url_image : 'img/not_found_image.png'}" class="card-img-top" alt="imagen de producto">
     <div class="card-body">
     <h5 class="card-title">${product.name}</h5>
     <p class="card-text">$${product.price}</p>
     </div>
     </div>
-    </div>`
+    </div>`);
 
-    )
-     
-    let url= "http://127.0.0.1:8000/api/product/"
-    fetch(url)
-        .then(response => response.json())
-        .then(data => mostrarData(data))
-        .catch(error => console.log(error))
-
-    const mostrarData = (data) =>{
-        console.log(data)
-        let body = ''
-        for (let i = 0; i < data.length; i++) {
-            body += cardProduct(data[i])
-            
-        }
-
-        document.getElementById('data').innerHTML = body
-
+const mostrarData = (data) => {
+    let content = document.getElementById('data');
+    content.innerHTML = "";
+    let body = '';
+    for (const product of data) {
+        body += cardProduct(product);
     }
+
+    content.innerHTML = body;
+}
+
+fetch(`${URI_BACKEND}/api/product`)
+    .then(response => response.json())
+    .then(data => mostrarData(data))
+    .catch(error => console.log(error))
+
+const searchInput = document.getElementById("search");
+
+searchInput.addEventListener('keyup', (event) => {
+    if (event.code == 'Enter') findProduct();
+})
+
+const findProduct = () => {
+    console.log("Buscando producto... " + searchInput.value);
+    fetch(`${URI_BACKEND}/api/searchProduct`, {
+        method: 'POST',
+        body: JSON.stringify({ search: searchInput.value }),
+        headers: { "Content-type": "application/json; charset=UTF-8" }
+
+    }).then(res => res.json())
+        .then(data => mostrarData(data))
+        .catch(error => console.log(error));
+}
